014543f897f6e3c895d6aae92425dca784fa75ee
Violet Tape <my@violet-tape.net>
2017-04-17 18:16:19 +0200

    Refactoring for tests and logic connections

---
+++ Digger/DiggerCoreTests/TileArrayTests/TileArrayTests.cs
@@ -0,0 +1,42 @@
+﻿using DiggerCore;
+using DiggerCore.ElementalStructures;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DiggerCoreTests {
+    [TestFixture]
+    [TestOf(typeof(TileArray))]
+    public class TileArrayTests {
+        [Test]
+        public void ShouldCreateDefaultTypeDirt() {
+            var tileArray = new TileArray(new Size(100, 100));
+
+            tileArray[1, 1]
+                    .Should()
+                    .NotBeNull();
+
+            tileArray[1, 1]
+                    .Type
+                    .Should()
+                    .Be(TileType.Dirt);
+        }
+
+        [Test]
+        public void CreateTest() {
+            var tileArray = new TileArray(new Size(2000, 50));
+
+            tileArray[1, 1]
+                    .Should()
+                    .NotBeNull();
+
+            tileArray[1, 1]
+                    .Type
+                    .Should()
+                    .Be(TileType.Dirt);
+        }
+
+       
+
+
+    }
+}
---
+++ Digger/DiggerCoreTests/TileArrayTests/WhenGetWindow.cs
@@ -0,0 +1,61 @@
+﻿using DiggerCore;
+using DiggerCore.ElementalStructures;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DiggerCoreTests {
+    [TestFixture]
+    [TestOf(typeof(TileArray))]
+    public class WhenGetWindow {
+        [Test]
+        public void SizeShouldBeAccordingToSetWindow()
+        {
+            // arrange
+            var tileArray = new TileArray(new Size(2000, 50));
+            var offset = new Point(5, 10);
+            tileArray.SetWindow(offset);
+
+            // act
+            var window = tileArray.GetWindow(new Point(20, 20));
+
+            // assert
+            window.Width.Should().Be(offset.Width*2);
+            window.Depth.Should().Be(offset.Depth*2);
+        }
+
+        [Test]
+        public void ShouldRepresentDataFromOriginalMap()
+        {   
+            // arrange
+            var tileArray = new TileArray(new Size(2000, 50));
+            var offset = new Point(5, 10);
+            tileArray.SetWindow(offset);
+
+            // pre-assert
+            tileArray[20 - 5, 20 - 10]
+                    .Type
+                    .Should()
+                    .Be(TileType.Dirt);
+
+            // act
+            var window = tileArray.GetWindow(new Point(20, 20));
+            window[0,0].Type = TileType.Empty;
+
+
+            // assert
+            tileArray[20 - 5, 20 - 10]
+                    .Type
+                    .Should()
+                    .Be(TileType.Empty);
+        }
+
+        [Test]
+        public void ShouldProcessEndOfMap() {
+            var tileArray = new TileArray(new Size(50, 50));
+            tileArray.SetWindow(new Point(5, 10));
+
+            var window = tileArray.GetWindow(new Point(4, 4));
+            window[0, 0].Type = TileType.Blacked;
+        }
+    }
+}
